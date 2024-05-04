7e353960c2e4a043665f3e70f9e3e624c4dfd5e3
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-05-04 21:03:05 +0530

    OUTPUT FUNCTION
    Python Print Function Can Take it Aribtary Number of Function .
    print("I am",34,"Years Old") 
    
    # Kitne NUmber Of Data Type print() ke aandar LIkh Sakte hain
    print(12, 3.4 , "String",4+5j)

--- 4May2024/4_may_2024.py
+++ 4May2024/4_may_2024.py
@@ -17,3 +17,20 @@ id(rollNO)
 x = 6
 x
 id(x)
+#Two Most Common Function performed by any Programming language
+#Input
+#Output
+#Processing
+#     **************************    OUTPUT FUNCTION  ***********************
+print("Good Evening Everyone!")   #  Any Number Of Argument We Can Pass in this
+print("I am ",20,"years old")     #  I am + 20 + years old = 3 Aribtary number
+print("I am",34,"Years Old")      #  I am + 34 + years old = 3 Aribtary number
+
+#  Note : Python Print Function Can Take it Aribtary Number of Function
+print("Pravin has", 14 , "Years of Exprience and he is",5.9,"ft tall")  # Pravin has + 14 + Years of Experience and he is + 5.9 + ft tall
+print(12, 3.4 , "String",4+5j)    # Kitne NUmber Of Data Type print() ke aandar LIkh Sakte hain
+
+
+
+
+
