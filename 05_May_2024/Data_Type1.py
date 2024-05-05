d1f47306feb60c620c1fccfba67d90213a69d582
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-05-05 13:18:20 +0530

    Representing Integers
    x = 0144 
    SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o(Zero and o Alphabet) prefix for octal integers

--- 05_May_2024/Data_Type1.py
+++ 05_May_2024/Data_Type1.py
@@ -6,3 +6,12 @@ print("Type of '4' :",type('4'))
 print("Type of 'A' :",type('A'))
 print("Type of \"ABC\":",type("ABC"))
 print("Type of 4j :",type(4j))
+
+#Representing Integers
+
+# decimal literal
+x = 100
+print("Type of" , x , " : ",type(x) )
+
+# x = 0144 (SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers )
+
