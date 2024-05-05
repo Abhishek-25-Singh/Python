05a0830906861f0cf88115456e932263104ad771
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-05-05 13:16:16 +0530

    Data Types in Python
    In Python we Don't Have any Data Type Called "Char"......
    Slash Use Karke Hum Uska Meaning Ko Change KAr skate Hain 
    print  (  "  Type  of  \   "  ABC  \   "   :      "  ,  type (  "  ABC   "   )   )

---
+++ 05_May_2024/Data_Type1.py
@@ -0,0 +1,8 @@
+#Type Function And Data Types in Python 
+
+print("Type of 4 :", type (4))
+print("Type of 4.0  :",type(4.0))
+print("Type of '4' :",type('4'))
+print("Type of 'A' :",type('A'))
+print("Type of \"ABC\":",type("ABC"))
+print("Type of 4j :",type(4j))
