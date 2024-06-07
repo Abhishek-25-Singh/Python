7d625b820bc564828a8da46acdfa237dcb1f0ef5
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-07 11:31:28 +0530

    Sequence, Addition of Two integer
    program for Calculation Addition of Two integer.

---
+++ 6_june_2024/Sequence.py
@@ -0,0 +1,6 @@
+# Build Program Using Sequence Construct 
+#program for Calculation Addition of Two integer 
+number1 = int(input("Please Enter The First  Integer : "))
+number2 = int(input("Please Enter The Second Integer : "))
+addition = number1 + number2 
+print(number1 ,"+",number2,"=",addition)
