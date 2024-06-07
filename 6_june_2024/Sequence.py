999db17b0f53d15235a00cbd3410468cfa252b1d
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-07 11:35:28 +0530

    Given The Radius of a Circle Find The Area and Circumference
    "We have used the float data type and the value of Pi (3.14), but the value is not precise, and we also did not mention the units of the parameter in the final result."

--- 6_june_2024/Sequence.py
+++ 6_june_2024/Sequence.py
@@ -4,3 +4,10 @@ number1 = int(input("Please Enter The First  Integer : "))
 number2 = int(input("Please Enter The Second Integer : "))
 addition = number1 + number2 
 print(number1 ,"+",number2,"=",addition)
+
+# Given The Radius of a Circle Find The Area and Circumference
+radius = float(input("Please Enter The Radius Of The Circle in cm : "))
+area_circle = 3.14 *(radius*radius)
+Circumference = 2*3.14*radius
+print("Area Of Circle With Radius ",radius,"is",area_circle)
+print("Circumference Of Circle With Radius ",radius,"is",Circumference)
