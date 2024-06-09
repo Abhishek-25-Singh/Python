bb2a85821a2863c62de5e07f0ac1a62a2eccd992
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-09 20:31:39 +0530

    module decimal

--- 6_june_2024/Float.py
+++ 6_june_2024/Float.py
@@ -28,6 +28,15 @@ print("Type of",x,":",type(x))
 print("Is",x,"Of integer type ?",isinstance(x,int))
 print("Is",x,"Of integer type ?",isinstance(x,float))
 
+
+#module decimal
+#Module Names are in SmallCase & Class and Type are in Uppercase eg: Decimal
+n1 = 0.1 + 0.1 + 0.1 
+n2 = 0.3
+print(n1==n2)
+print((0.1+0.1+0.1)==(0.3))
+print(type(n1),type(n2))
+
 print()
 x = 7.0
 print("Type of",x,":",type(x))
