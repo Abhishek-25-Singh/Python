f29400368b1b1c1455b6fb1ef91a64b8d71e4be4
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-09 19:58:11 +0530

    Repreenting Floating point in Exponential(Scientific) Format
    you can represent floating point numbers in exponential (or scientific) notation
    
    *     700000000000000.0 is a floating-point number written in 
           standard notation.
    *     7.0E+14 is the same number written in exponential (scientific) 
          notation.
          x = 7.0E+14: This assigns 7.0 times 10^14 to x, which is 
          equivalent to 700000000000000.0
    
      x = 700000000000000.0 and  x = 7.0E+14 are represented as 
      floating-point numbers in Python.
      
     7.0E+14 is essentially the same as 700000000000000.0. It's just 
     another representation of the same numerical value, using 
     scientific notation. So, both assignments result in the same 
     numerical value and type.

--- 6_june_2024/Float.py
+++ 6_june_2024/Float.py
@@ -2,3 +2,14 @@
 print("type of 4.5 :" , type(4.5))
 print("Type of 45  : ", type(45))
 
+#Repreenting Floating point in Exponential(Scientific) Format
+x = 7.2
+print("x = ",x,"type = " ,type(x))
+x=700000000000000.0
+print("x = ",x,"type = " ,type(x))
+x=7.0E+14
+print("x = ",x,"type = " ,type(x))
+x=4.3E-3
+print("x = ",x,"type = " ,type(x))
+x=4.3E+3
+print("x = ",x,"type = " ,type(x))
