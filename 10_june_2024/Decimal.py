4dac04945731f8cb42d351e22fa61347cf1fa506
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-10 23:34:29 +0530

    module decimal
    n1 = 0.1 + 0.1 + 0.1 
    n2 = 0.3
    print ( n1 = =  n2  )
    print  (  (0.1  +  0.1  +  0.1  )   =  =  (  0.3  )   ) 
    print  (  type  (  n1  )  ,  type  (  n2  )  )
    
    Output:
    False
    False
    < class ' float ' > < class ' float ' >
    In Python, due to floating-point precision issues, calculations involving floating-point numbers can sometimes produce unexpected results. 
    Let's run the provided code and explain the output:
    
    n1 = 0.1 + 0.1 + 0.1 
    n2 = 0.3
    print ( n1 = =  n2  )
    print  (  (0.1  +  0.1  +  0.1  )   =  =  (  0.3  )   ) 
    print  (  type  (  n1  )  ,  type  (  n2  )  )
    
    Explanation:
    1.Floating-Point Precision:
         *    n1 = 0.1 + 0.1 + 0.1 results in a value that might not be 
              exactly equal to 0.3 due to floating-point arithmetic 
              precision issues.
        *    n2 = 0.3 is straightforward but may not match n1 exactly in 
              binary representation.

---
+++ 10_june_2024/Decimal.py
@@ -0,0 +1,8 @@
+#module decimal
+#Module Names are in SmallCase & Class and Type are in Uppercase eg: Decimal
+
+n1 = 0.1 + 0.1 + 0.1 
+n2 = 0.3
+print(n1==n2)
+print((0.1+0.1+0.1)==(0.3))
+print(type(n1),type(n2))
