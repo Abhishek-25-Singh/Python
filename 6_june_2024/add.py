8a8874e32c5378fa82c617952e6c3495ac5ae96f
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-17 00:01:20 +0530

    Add files via upload

---
+++ 6_june_2024/add.py
@@ -0,0 +1,5 @@
+#program for Calculation Addition of Two integer 
+number1 = int(input("Please Enter The First  Integer : "))
+number2 = int(input("Please Enter The Second Integer : "))
+addition = number1 + number2 
+print(number1 ,"+",number2,"=",addition)
