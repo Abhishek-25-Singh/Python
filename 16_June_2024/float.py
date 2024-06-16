d9a08a381f70b57eedc8829d6aaeed4d546e633a
Abhishek SIngh <92333269+Abhishek-25-Singh@users.noreply.github.com>
2024-06-17 00:04:57 +0530

    type() function

---
+++ 16_June_2024/float.py
@@ -0,0 +1,13 @@
+#int rollNo;
+#float Perc;
+
+
+#Checking type of the variable
+rollNo = 5
+perc = 77.78
+print("Type of Roll No    : ", type(rollNo))
+print("Type of percentage : ", type(perc))
+
+#Checking Type of Values 
+print("Type of 5 :",type(5))
+print("Type of 5.5 :",type(5.5))
